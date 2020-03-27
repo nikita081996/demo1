0626bd9935c3eb8fa31d7cb10b6e89579d9c5bc2
nikita0896 <39641014+nikks081996@users.noreply.github.com>
2020-03-27 12:23:18 +0530

    merged one with two

--- app/src/main/java/com/example/demo1/MainActivity.kt
+++ app/src/main/java/com/example/demo1/MainActivity.kt
@@ -5,11 +5,8 @@ import android.os.Bundle
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
-<<<<<<< HEAD
     var two:TextView? = null
-=======
     var one: TextView? = null
->>>>>>> one
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
