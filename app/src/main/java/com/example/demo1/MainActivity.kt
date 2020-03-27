e71715bddc488878b269b858b63437894b017512
nikita0896 <39641014+nikks081996@users.noreply.github.com>
2020-03-27 12:04:51 +0530

    first_commit

--- app/src/main/java/com/example/demo1/MainActivity.kt
+++ app/src/main/java/com/example/demo1/MainActivity.kt
@@ -2,8 +2,10 @@ package com.example.demo1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    var one: TextView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
